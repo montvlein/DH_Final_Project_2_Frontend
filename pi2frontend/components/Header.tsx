e01ba51abf2a8f0218c96fac118025a5014ee47d
Mili Cerverizzo <milicerve@hotmail.com>
2023-10-19 15:23:47 -0300

    create navbar responsive

--- pi2frontend/components/Header.tsx
+++ pi2frontend/components/Header.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { CiMenuFries } from 'react-icons/ci'
+
 const Header: React.FC = () => {
   return (
-    <div>
-      Header
+    <div className='w-full  absolute z-10'>
+      <nav className='h-118 mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-black shadow-md'>
+        <Link href='/' className='flex justify-center items-center'>
+        <Image
+          src='/Icon Design Logo.svg'
+          alt='logo'
+          width={295}
+          height={92}
+          className='object-contain'
+        />
+        </Link>
+        <CiMenuFries size= '2rem'color="white" className="md:hidden"/>
+        <div className= "hidden md:block">
+          <div className="flex space-x-10">
+            <Link href='/' className= "text-white font-montserrat text-base font-normal">Inicio</Link>
+            <Link href='/' className= "text-white font-montserrat text-base font-normal">Soporte</Link>
+            <Link href='/' className= "text-white font-montserrat text-base font-normal">Inicia Sesión</Link>4
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
