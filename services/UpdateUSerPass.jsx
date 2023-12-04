9079e15dca9b8720d6f3a012a1bb9dc1d119696d
montvlein <montvlein@gmail.com>
2023-12-04 00:26:46 -0300

    se corrigio funcion para cambiar el password del usuario

---
+++ services/UpdateUSerPass.jsx
@@ -0,0 +1,28 @@
+import { GoldenApi } from '@/api/data'
+
+export default async function UpdateUserPass({currentPassword, newPassword}) {
+  const baseApi = GoldenApi.base
+  const endpoint = GoldenApi.endoints.user.changePass
+  const storedToken = localStorage.getItem('token')
+  if (storedToken) {
+    try {
+      const opt = {
+        method: 'PATCH',
+        headers: {
+          token: storedToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ currentPassword, newPassword })
+      }
+      const response = await fetch(baseApi + endpoint, opt)
+      if (!response.ok) {
+        throw new Error('Error en la carga de usuario')
+      }
+      return await response.json()
+
+    } catch (error) {
+      console.error('Error al obtener la información del usuario:', error);
+    }
+  }
+  return null
+}
