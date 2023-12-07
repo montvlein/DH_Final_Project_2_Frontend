40fc80d776e87958461e4bf241cb5ecc45e8dbdd
montvlein <montvlein@gmail.com>
2023-12-07 20:51:07 -0300

    se agrego equeleto

--- components/profile/MisEntradas.tsx
+++ components/profile/MisEntradas.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import GetTickets from '../../services/GetTikets'
-import Spinner from '../Spinner'
 
 const MisEntradas: React.FC = () => {
   const param = useParams()
@@ -19,7 +18,7 @@ const MisEntradas: React.FC = () => {
       .then((eList: any[]) => {
         const userTicketsList = eList.filter(t => parseInt(t.idUser) === parseInt(id))
         setEventList(userTicketsList)
-        setLoading(false)
+        setLoading(true)
       })
   }
 
@@ -39,7 +38,10 @@ const MisEntradas: React.FC = () => {
           <button className=" py-4 uppercase font-semibold lg:text-2xl text-xs  text-[#975D93] border-2 border-transparent border-b-[#DCA6D8] ">Mis entradas</button>
         </div>
         <div className="p-8 flex flex-col gap-4">
-          { loading ? <Spinner/> : eventList.map((ticket, index) => (
+          { loading ?
+            <div className='h-[50vh]'>
+              <Skeleton />
+            </div>   : eventList.map((ticket, index) => (
             <CardsProfile key={index} ticket={ticket} />
             ))
           }
@@ -50,3 +52,22 @@ const MisEntradas: React.FC = () => {
   )
 }
 export default MisEntradas
+
+const Skeleton: React.FC = () => {
+  return(
+    <article className='animate-pulse rounded-2xl bg-slate-900 overflow-hidden shadow-xl  md:mx-24 my-2'>
+        <div className='bg-slate-600 w-full h-48'></div>
+        <div className='p-4 flex gap-6 bg-gray-100 dark:bg-gray-700'>
+          <p className='flex flex-col items-center font-bold'>
+            <span className='text-blue-500 bg-slate-600 w-8 h-8 rounded'></span>
+            <span className='text-3xl bg-slate-600 w-8 h-8 rounded'></span>
+          </p>
+          <div className='text-base w-full'>
+            <h4 className='mb-2 bg-slate-600 h-8 w-24 rounded'></h4>
+            <p className='text-sm font-normal h-24 rounded w-full bg-slate-600 text-gray-600 dark:text-gray-100'>
+            </p>
+          </div>
+        </div>
+    </article>
+  )
+}
